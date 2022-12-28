f7dc401ce1f91d1c1fd5f6b019bcf68f1996030e
Hans Castro <hanscastroadmn@gmail.com>
2022-12-28 01:15:06 +0100

    [Project] Implemented CreateUserProjectAction.py

--- src/Project/Application/CreateUserProjectAction.py
+++ src/Project/Application/CreateUserProjectAction.py
@@ -3,8 +3,10 @@ from src.Project.Infrastructure.IProjectRepository import IProjectRepository
 
 class CreateUserProjectAction:
 
-    def __init__(self, project_repository: IProjectRepository, name, url):
-        pass
+    def __init__(self, project_repository: IProjectRepository, name: str, url: str):
+        self.project_repository = project_repository
+        self.name = name
+        self.url = url
 
     def run(self):
-        pass
+        self.project_repository.create(self.name, self.url)
